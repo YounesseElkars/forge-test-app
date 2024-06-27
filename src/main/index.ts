6d730cccbf5950ae8e9e475a79a94c695577f6a3
Younesse Elkars <97364255+YounesseElkars@users.noreply.github.com>
2024-06-27 17:16:12 +0100

    views

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,72 +1,96 @@
-import { app, shell, BrowserWindow, ipcMain, WebContentsView, BaseWindow } from 'electron';
+import { app, shell, BrowserWindow, ipcMain } from 'electron';
 import { join } from 'path';
 import { is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
 import { screen } from 'electron';
 import path from 'path';
+import WebViewManager from './WebViewManager';
 const { localStorage } = require('electron-browser-storage');
 
 process.env.VITE_PUBLIC = path.join(__dirname, '../renderer/public');
 
-let win: BrowserWindow;
+class MainApp {
+  private win: BrowserWindow | null = null;
+  WebViewManager: WebViewManager | null = null;
+  constructor() {
+    app.whenReady().then(this.createWindow.bind(this));
+    app.on('activate', this.onActivate.bind(this));
+    this.initializeIPC();
+  }
 
-function createWindow(): void {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  initSkeleton(win) {
+    this.WebViewManager = new WebViewManager(win, { x: 400, y: 30, width: 400, height: 600 });
+  }
 
-  win = new BrowserWindow({
-    width: width,
-    height: height,
-    frame: true,
-    fullscreen: false,
-    show: false,
-    autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
-    webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false,
-      devTools: true,
-      nodeIntegration: true,
-    },
-  });
+  private createWindow(): void {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
-  win.on('ready-to-show', async () => {
-    win?.show();
-    await localStorage.setItem('favorite_number', 12);
-  });
+    this.win = new BrowserWindow({
+      width,
+      height,
+      frame: true,
+      fullscreen: false,
+      show: false,
+      autoHideMenuBar: true,
+      ...(process.platform === 'linux' ? { icon } : {}),
+      webPreferences: {
+        preload: join(__dirname, '../preload/index.js'),
+        sandbox: false,
+        devTools: true,
+        nodeIntegration: true,
+      },
+    });
 
-  win.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url);
-    return { action: 'deny' };
-  });
+    this.initSkeleton(this.win);
 
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    win.loadURL(process.env['ELECTRON_RENDERER_URL']);
-  } else {
-    win.loadFile(join(__dirname, '../renderer/index.html'));
+    this.win.on('ready-to-show', async () => {
+      this.win?.show();
+      await localStorage.setItem('favorite_number', 12);
+    });
+
+    this.win.webContents.setWindowOpenHandler((details) => {
+      shell.openExternal(details.url);
+      return { action: 'deny' };
+    });
+
+    if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+      this.win.loadURL(process.env['ELECTRON_RENDERER_URL']);
+    } else {
+      this.win.loadFile(join(__dirname, '../renderer/index.html'));
+    }
+  }
+
+  private onActivate(): void {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      this.createWindow();
+    }
+  }
+
+  private initializeIPC(): void {
+    ipcMain.on('open-base-window', (_event, params: string) => {
+      this.WebViewManager?.addWebView(params);
+    });
+
+    ipcMain.on('show-base-window', (_event, params: string) => {
+      this.WebViewManager?.showWebView(params);
+    });
+
+    ipcMain.on('hide-base-window', (_event, params: string) => {
+      this.WebViewManager?.hideWebView(params);
+    });
+
+    ipcMain.on('remove-base-window', (_event, _params: string) => {
+      this.WebViewManager?.removeAllWebViews();
+    });
+
+    ipcMain.on('change-url-base-window', (_event, params: string) => {
+      this.WebViewManager?.changeURL(params);
+    });
+
+    ipcMain.on('refresh-view', (_event, params: string) => {
+      this.WebViewManager?.refreshView(params);
+    });
   }
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-ipcMain.on('open-base-window', () => {
-  const basewin = new BaseWindow({ width: 800, height: 600 });
-
-  const leftView = new WebContentsView();
-  leftView.webContents.loadURL('https://electronjs.org');
-  basewin.contentView.addChildView(leftView);
-
-  const rightView = new WebContentsView();
-  rightView.webContents.loadURL('https://github.com/electron/electron');
-  basewin.contentView.addChildView(rightView);
-
-  leftView.setBounds({ x: 0, y: 0, width: 400, height: 600 });
-  rightView.setBounds({ x: 400, y: 0, width: 400, height: 600 });
-});
+new MainApp();
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import { Button } from './components/ui/button';
 
 function App() {
+  const [text, setText] = useState('http://facebook.com');
   return (
     <div>
-      <Button onClick={() => window.electron.ipcRenderer.send('open-base-window')}>open base window</Button>
+      <Button onClick={() => window.electron.ipcRenderer.send('open-base-window', text)}>open </Button>
+      <Button onClick={() => window.electron.ipcRenderer.send('show-base-window', text)}>show</Button>
+      <Button onClick={() => window.electron.ipcRenderer.send('hide-base-window', text)}> hide </Button>
+      <Button onClick={() => window.electron.ipcRenderer.send('remove-base-window', text)}> remove </Button>
+      <Button onClick={() => window.electron.ipcRenderer.send('change-url-base-window', text)}>change url</Button>
+      <Button onClick={() => window.electron.ipcRenderer.send('refresh-view', text)}>refresh view</Button>
+      <br />
+
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} className="bg-black text-white" />
     </div>
   );
 }
